3f404074f936f71f4bed4b72f910dfd6ab5310ab
E(Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& c:\Users\aapoh\Desktop\python-harjoitusket\pythonharjoitukset\Scripts\Activate.ps1)i <aapohannikainen@gmail.com>
2026-08-20 15:26:33 +0300

    ä

---
+++ pythonharjoitukset/tuntiharjoitukset/tuntitehtävä3.py
@@ -0,0 +1,10 @@
+gramma = int(input("Kuinka monta grammaa?"))
+          
+kilogramma = gramma/1000
+
+uusi_gramma = gramma-int (kilogramma)*1000
+
+print (int(kilogramma), " kiloa ", uusi_gramma, " grammaa ")
+
+
+
