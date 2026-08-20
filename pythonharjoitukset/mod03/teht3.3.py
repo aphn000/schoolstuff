2623dc8c7e7b1c806f12958efc7ad6429762ec59
E(Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& c:\Users\aapoh\Desktop\python-harjoitusket\pythonharjoitukset\Scripts\Activate.ps1)i <aapohannikainen@gmail.com>
2026-08-20 13:42:03 +0300

    öööö

---
+++ pythonharjoitukset/mod03/teht3.3.py
@@ -0,0 +1,8 @@
+
+import math
+kanta = input("Anna suorakulmion kanta")
+korkeys = input("Anna suorakulmion korkeus")
+piiri = math kanta*2+korkeys*2
+pinta_ala = math kanta*korkeys
+print("Suorakulmion piiri on", piiri) 
+print("Suorakulmion pinta-ala on", pinta_ala)
---
+++ pythonharjoitukset/tarinaharjoitus.py
@@ -0,0 +1,4 @@
+nimi = input("Anna nimi:")
+ikä = input("Anna ikä:")
+adjektiivi = input("Anna adjektiivi:")
+print(nimi + " oli " + adjektiivi +  " henkilö. Hän oli " + ikä + " vuotta vanha ja todella " + adjektiivi + " vaikka hän oli Kouvolasta.")
