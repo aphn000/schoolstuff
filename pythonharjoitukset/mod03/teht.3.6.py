2b1c7420dd2664b53709cb69cffb888beb1b2728
Aapo Hannikainen <aapohannikainen@gmail.com>
2026-08-23 21:32:59 +0300

    koodi

---
+++ pythonharjoitukset/mod03/teht.3.6.py
@@ -0,0 +1,11 @@
+import random
+
+koodi1 = (random.randint(0,9))
+koodi2 = (random.randint(0,9))
+koodi3 = (random.randint(0,9))
+numkoodi1 = (random.randint(0,6))
+numkoodi2 = (random.randint(0,6))
+numkoodi3 = (random.randint(0,6))
+numkoodi4 = (random.randint(0,6))
+
+print("Kolmenumeroinen koodi on ", koodi1, koodi2, koodi3, "ja nelinumeroinen koodi on ", numkoodi1, numkoodi2, numkoodi3, numkoodi4 )
