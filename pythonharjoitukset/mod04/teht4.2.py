1a1c1e9279723328fef30ba82b96d1755e0b0f3b
Aapo Hannikainen <aapohannikainen@gmail.com>
2026-08-23 23:38:25 +0300

    WOW!

---
+++ pythonharjoitukset/mod04/teht4.2.py
@@ -0,0 +1,9 @@
+hyttiluokka = input("Kerro hyttiluokkasi.")
+
+
+
+if hyttiluokka == "LUX": print("LUX on parvekkeellinen hytti yläkannella.")
+elif hyttiluokka == "A": print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hyttiluokka == "B": print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hyttiluokka == "C": print("C on ikkunaton hytti autokannen alapuolella.")
+else: print("Virheellinen hyttiluokka.")
