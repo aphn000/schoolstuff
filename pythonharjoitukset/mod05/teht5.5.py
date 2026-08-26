19eca34d28a929e84518b8c2ae1d59f631869390
E(Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& c:\Users\aapoh\Desktop\python-harjoitusket\pythonharjoitukset\Scripts\Activate.ps1)i <aapohannikainen@gmail.com>
2026-08-26 14:24:55 +0300

    ö

--- pythonharjoitukset/mod05/teht5.5.py
+++ pythonharjoitukset/mod05/teht5.5.py
@@ -1,8 +1,14 @@
+print("Kerro salasana ja käyttäjätunnus.")
+kerrat = 0
 
-kerrat = 1
-
-while = True:
-    kayttajatunnus = input("Anna käyttäjätunnus.")
-    salasana= int(input("Anna salasana."))
+while kerrat < 5:
+    kayttajatunnus = input("Anna käyttäjätunnus. ")
+    salasana= (input("Anna salasana. "))
+    if salasana == "rules" and kayttajatunnus == "python": 
+        print("Tervetuloa!")
+        break
+    else: 
+        print("Pääsy evätty. Yritä uudelleen.")
+        kerrat +=1
 
     
