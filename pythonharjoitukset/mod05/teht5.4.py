ca996c429a31223d64a09f9e2984abbe8b8d5bdb
E(Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned) ; (& c:\Users\aapoh\Desktop\python-harjoitusket\pythonharjoitukset\Scripts\Activate.ps1)i <aapohannikainen@gmail.com>
2026-08-26 11:43:14 +0300

    ö

--- pythonharjoitukset/mod05/teht5.4.py
+++ pythonharjoitukset/mod05/teht5.4.py
@@ -1,5 +1,16 @@
 import random
 
-luku=int(input("Arvaa luku"))
+arvottunumero = random.randint(1, 10)
+arvaus= 0
 
-while 
+while arvottunumero != arvaus:
+    arvaus = int(input("Arvaa luku väliltä 1-10.  "))
+
+    if arvaus == arvottunumero:
+        print("Arvasit oikean numeron.")
+    elif arvaus >= arvottunumero:
+        print ("Liian suuri arvaus.")
+    elif arvaus <= arvottunumero:
+        print("Liian pieni arvaus.")
+    
+    
