f0e6efad08612f77051b46e01053485d40e6873e
Aapo Hannikainen <aapohannikainen@gmail.com>
2026-09-02 22:47:32 +0300

    ööö

--- pythonharjoitukset/mod05/teht5.6.py
+++ pythonharjoitukset/mod05/teht5.6.py
@@ -1,5 +1,20 @@
 import random
-import math
+
 
 N=int(input("Kerro kulmapisteiden määrä."))
 
+i = 0
+n = 0
+
+while i < N:
+    x = random.randint(-1, 1)
+    y = random.randint(-1, 1)
+
+    if x**2 + y**2 < 1:
+        n += 1
+
+    i += 1
+
+piin_arvo = 4 * n / N
+
+print("Piin likiarvo on", piin_arvo)
