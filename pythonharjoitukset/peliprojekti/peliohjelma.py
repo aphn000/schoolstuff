c87ac96e8eb52f7dac3547071b87f21e61015b5e
Aapo Hannikainen <aapohannikainen@gmail.com>
2026-09-09 01:10:28 +0300

    ööööö

--- pythonharjoitukset/peliprojekti/peliohjelma.py
+++ pythonharjoitukset/peliprojekti/peliohjelma.py
@@ -8,22 +8,51 @@ while ika <12:
     print ("Olet alaikäinen ahahahahahh")
     break
 else:
-    print ("Hei ", nimi,"!")
-    print ("Tervetuloa peliin.")
-    print ("Voit seurata peliin liittyviä asioitasi päävalikossa. Komennolla PELAAJA saat tiedot pelaajastasi. Komennolla SAAVUTUKSET saat tiedot saamistasi saavutuksista. Komennolla LOPETA peli päättyy.")
+    print("Hei ", nimi, "!")
+    print("Tervetuloa peliin.")
+    print("Voit seurata peliin liittyviä asioitasi päävalikossa.")
+    print("Komennolla LISÄÄ voit lisätä esineitä pelaajasi tavaraluetteloon.")
+    print("Komennolla TAVARALUETTELO voit tarkastella pelaajasi tavaraluetteloa.")
+    print("Komennolla APUA saat ohjeita pelin toimintaan.")
+    print("Komennolla LOPETA peli päättyy.")
 
-paavalikko1 = "PELAAJA".upper()
-paavalikko2 = "SAAVUTUKSET".upper()
-paavalikko3 = "LOPETA".upper()
+esineet = []
+
+def lisaa_esine():
+    esine = input("Kerro, minkä esineen haluat antaa pelaajallesi.")
+    esineet.append(esine)
+    print("Esine on lisätty tavaraluetteloon.")
+
+
+def nayta_esineet():
+    print("Pelaajallasi on seuraavat esineet")
+
+    for esine in esineet:
+        print(esine)
+
+def apua():
+    print("LISÄÄ = lisää esine tavaraluetteloon")
+    print("TAVARALUETTELO = näyttää pelaajan tavaraluettelon")
+    print("APUA = näyttää pelin ohjeet")
+    print("LOPETA = lopettaa pelin ja sulkee ohjelman")
+
+
+paavalikko1 = "LISÄÄ".upper()
+paavalikko2 = "TAVARALUETTELO".upper()
+paavalikko3 = "APUA".upper()
+paavalikko4 = "LOPETA".upper()
 
 
 while True:
 
-    valinta = input("Kerro valintasi.").upper()
+    valinta=input("Kerro valintasi.").upper()
+
     if valinta == paavalikko1:
-        print("Pelaajan nimi on", nimi, "ja pelaajan ikä on", ika, ". Emme tiedä vielä muuta pelaajasta")
-    elif valinta == paavalikko2: 
-        print("Pelaajalla ei ole vielä saavutuksia, sillä peli on kehitysvaiheessa.")
+        lisaa_esine()
+    elif valinta == paavalikko2:
+        nayta_esineet()
     elif valinta == paavalikko3:
+        apua()
+    elif valinta == paavalikko4:
         print("Peli sammuu")
         break
