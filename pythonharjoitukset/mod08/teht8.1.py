3c3d1bcb1348e06866af60c5f68514d5e2b8333c
Aapo Hannikainen <aapohannikainen@gmail.com>
2026-09-20 22:13:41 +0300

    yoo

---
+++ pythonharjoitukset/mod08/teht8.1.py
@@ -0,0 +1,5 @@
+vuodenajat = ("talvi", "talvi", "kevät", "kevät","kevät", "kesä", "kesä", "kesä", "syksy", "syksy", "syksy", "talvi")
+kuukausi = int(input("Kerro kuukausien numero."))
+vuodenajat[kuukausi-1]
+vuodenajat[1 - 1]
+print(vuodenajat[kuukausi - 1])
