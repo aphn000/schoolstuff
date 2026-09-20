64630785cc9c6c9458d9132534edde4e8ca4b682
Aapo Hannikainen <aapohannikainen@gmail.com>
2026-09-20 22:25:07 +0300

    ööö

--- pythonharjoitukset/mod08/teht8.2.py
+++ pythonharjoitukset/mod08/teht8.2.py
@@ -1,3 +1,17 @@
 nimet=set()
-while True
-    nimi=input("Anna nimi, tyhjä")
+
+while True:
+    nimi=input("Anna nimi: ")
+    if nimi == "":
+        break
+    if nimi in nimet:
+        print ("Aiemmin syötetty nimi.")
+    else:
+        print("Uusi nimi.")
+        nimet.add(nimi)
+print("Nimet ovat: ")       
+for nimi in nimet:
+    print(nimi)
+
+
+
